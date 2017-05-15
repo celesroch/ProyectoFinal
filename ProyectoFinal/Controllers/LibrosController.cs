f915df0eb1bffccc830f3dc64fe6791ed8e88110
Celeste Roche <celesroch@gmail.com>
2017-05-15 22:40:35 +0200

    Proyecto Final Signed-off-by: Celeste Roche <celesroch@gmail.com>

---
+++ ProyectoFinal/Controllers/LibrosController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DataLayer;
+
+namespace ProyectoFinal.Controllers
+{
+    public class LibrosController : Controller
+    {
+        private AlquilerLibrosEntities db = new AlquilerLibrosEntities();
+
+        // GET: Libros
+        public ActionResult Index()
+        {
+            return View(db.Libros.ToList());
+        }
+        // GET: Libros
+        public ActionResult MostrarLibros()
+        {
+            return View(db.Libros.ToList());
+        }
+
+        // GET: Libros/Details/5
+        public ActionResult Details1(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libros libros = db.Libros.Find(id);
+            if (libros == null)
+            {
+                return HttpNotFound();
+            }
+            return View(libros);
+        }
+        public ActionResult Details(int Id)
+        {
+            Libros libro = new Libros();
+            libro = db.Libros.Find(Id);
+            return PartialView("_Details", libro);
+        }
+        // GET: Libros/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Libros/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idLibro,nomLibro,ISBN,autor,editorial,comentario")] Libros libros)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Libros.Add(libros);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(libros);
+        }
+
+        // GET: Libros/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libros libros = db.Libros.Find(id);
+            if (libros == null)
+            {
+                return HttpNotFound();
+            }
+            return View(libros);
+        }
+
+        // POST: Libros/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idLibro,nomLibro,ISBN,autor,editorial,comentario")] Libros libros)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(libros).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(libros);
+        }
+
+        // GET: Libros/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libros libros = db.Libros.Find(id);
+            if (libros == null)
+            {
+                return HttpNotFound();
+            }
+            return View(libros);
+        }
+
+        // POST: Libros/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Libros libros = db.Libros.Find(id);
+            db.Libros.Remove(libros);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
